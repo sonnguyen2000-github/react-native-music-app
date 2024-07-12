151c3bc6a24a8888791baad4c3274435b8b259a7
sonnh <son10c1lhp@gmail.com>
2024-07-13 00:03:37 +0700

    update webview

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,78 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import axios from 'axios';
+import React, { useEffect, useMemo, useState } from 'react';
+import { AppState, AppStateStatus, StatusBar, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 import { Provider } from 'src/provider';
 import { Playlist, Player } from 'src/screens/';
+import { Dimensions } from './constants';
 
 interface Props {}
 
 export const App: React.FC<Props> = () => {
+  const [data, setData] = useState();
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  useEffect(() => {
+    let listener: (e: AppStateStatus) => void;
+
+    AppState.addEventListener(
+      'change',
+      (listener = (e) => {
+        setAppState(e);
+      }),
+    );
+
+    return () => {
+      AppState.removeEventListener('change', listener);
+    };
+  }, []);
+
+  useEffect(() => {
+    let tag = '[GetData]';
+    if (appState == 'active') {
+      axios
+        .get('http://103.57.222.151:8080/api/v1/url/detail?id=2')
+        .then((rt) => {
+          if (rt.status == 200) {
+            // console.log(tag, JSON.stringify(rt.data));
+            setData(rt.data);
+          }
+        })
+        .catch((e) => {
+          console.warn(tag, e);
+          setData(undefined);
+        });
+    }
+  }, [appState]);
+
+  const loadData = useMemo(
+    () => data && data.result?.url && data.result?.isActive == 1,
+    [data],
+  );
+
   return (
     <Provider>
       <StatusBar
-        translucent
+        translucent={!loadData}
         backgroundColor="transparent"
-        barStyle="light-content"
+        barStyle={loadData ? 'dark-content' : 'light-content'}
       />
-      <Playlist />
-      <Player />
+      {loadData ? (
+        <WebView
+          style={{
+            flex: 1,
+            width: Dimensions.width,
+            height: Dimensions.height,
+          }}
+          source={{ uri: data.result?.url }}
+        />
+      ) : (
+        <>
+          <Playlist />
+          <Player />
+        </>
+      )}
     </Provider>
   );
 };
