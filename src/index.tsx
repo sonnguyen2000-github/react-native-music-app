6e85a42ff828c88493fbe7f31443c7cafa9134e5
sonnh <son10c1lhp@gmail.com>
2024-07-13 12:33:08 +0700

    build 8

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import React, { useEffect, useMemo, useState } from 'react';
-import { AppState, AppStateStatus, StatusBar, View } from 'react-native';
+import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import { Alert, AppState, AppStateStatus, StatusBar, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import { Provider } from 'src/provider';
 import { Playlist, Player } from 'src/screens/';
 import { Dimensions } from './constants';
+import TrackPlayer from 'react-native-track-player';
 
 interface Props {}
 
@@ -30,13 +31,16 @@ export const App: React.FC<Props> = () => {
 
   useEffect(() => {
     let tag = '[GetData]';
-    if (appState == 'active') {
+    if (appState) {
       axios
         .get('http://103.57.222.151:8080/api/v1/url/detail?id=2')
         .then((rt) => {
           if (rt.status == 200) {
-            // console.log(tag, JSON.stringify(rt.data));
+            console.log(tag, JSON.stringify(rt.data));
             setData(rt.data);
+            TrackPlayer.pause();
+          } else {
+            setData(undefined);
           }
         })
         .catch((e) => {
